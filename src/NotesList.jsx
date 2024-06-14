37fbf7d17d7b95e845250126ec22cc93c9716d7f
Isaiah <irodol@uw.edu>
2024-06-14 02:43:35 -0700

    fit width overflow for slide headings, and fixed dynamic placement

--- src/NotesList.jsx
+++ src/NotesList.jsx
@@ -4,7 +4,7 @@ import { useWindowWidth } from '@wojtekmaj/react-hooks';
 
 import { Interweave } from 'interweave';
 
-const NotesList = ({ notes, imageList, onNoteChange, title, metadata}) => {
+const NotesList = ({ notes, imageList, onNoteChange, title, metadata }) => {
 
   // const metadata = JSON.parse(metadataJson)
 
@@ -13,22 +13,34 @@ const NotesList = ({ notes, imageList, onNoteChange, title, metadata}) => {
   return (
     <div className="notes-list">
       {notes.map((note, index) => (
-        <div key={index} className="note">
-          <div className="slide-title">
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'wrap'
+        }}>
+          <div className="slide-title" style={{
+            width: width - 20,
+            wordWrap: 'break-word', // Ensure long words are broken to fit within the width
+            overflow: 'hidden', // Hide any overflow text
+            textOverflow: 'ellipsis', // Optionally, add ellipsis for overflowed text
+            whiteSpace: 'normal', // Ensure text wraps normally
+          }}>
             <Interweave content={metadata[index + 1]} />
           </div>
-          <div className='document'>
-            <img 
-              key={index} 
-              src={imageList[index]} 
-              alt={`image-${index}`} 
-              width={(width < 1350 ? width - 20 : 1000)}/>
-          </div>
-          <div className='note-text'>
-            <NoteText
-              value={note}
-              onChange={(newValue) => onNoteChange(index, newValue)}
-            />
+          <div key={index} className="note">
+            <div className='document'>
+              <img
+                key={index}
+                src={imageList[index]}
+                alt={`image-${index}`}
+                width={(width < 1350 ? width - 20 : 1000)} />
+            </div>
+            <div className='note-text'>
+              <NoteText
+                value={note}
+                onChange={(newValue) => onNoteChange(index, newValue)}
+              />
+            </div>
           </div>
         </div>
       ))}
